239c8cfa1fe4e9a9905dded3b16e4998ae1c67a2
NobleMode <laminhcokho@gmail.com>
2024-03-22 21:19:06 +0700

    update
    search

--- src/search/search.service.ts
+++ src/search/search.service.ts
@@ -1,15 +1,18 @@
-
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose'; 
-import { Model } from 'mongoose'; 
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { MemberEntity } from '../entities/member.entity';
+
 
 @Injectable()
 export class SearchService {
-    constructor(@InjectModel('YourModel') private readonly yourModel: Model<any>) { } 
+    constructor(
+        @InjectRepository(MemberEntity)
+        private readonly yourRepository: Repository<MemberEntity>,
+    ) {}
 
-    async search(query: string): Promise<any[]> {
-        
-        const results = await this.yourModel.find({ }).exec();
+    async search(query: string): Promise<MemberEntity[]> {
+        const results = await this.yourRepository.find();
         return results;
     }
 }
